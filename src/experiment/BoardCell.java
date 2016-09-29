e08abbbe6065a5008aadbbaccb7f64f75f4b711c
hujohnso <hujohnso@mymail.mines.edu>
2016-09-29 14:47:25 -0600

    Added skeletons to BoardCell, and IntBoard

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,27 @@
+package experiment;
+/*This class should have/do the following:
+ * 
+ * DONE: Two member variables of type int to represent the row and column.
+ * 
+ * ToDo: Add more fields later.
+ * 
+ * DONE: Should be in the package experiment.
+ * 
+ * 
+ * 
+ * */
+public class BoardCell {
+	private int row;
+	private int column;
+	
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+
+}
