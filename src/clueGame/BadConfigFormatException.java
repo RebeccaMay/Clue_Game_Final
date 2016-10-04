6f18225e1af507574e896142e574d494c8f660c8
basic <reecehughes@mymail.mines.edu>
2016-10-04 12:51:27 -0600

    Created BadConfigFormatException.java, FileInitTest.java and OurTest.txt.  All of Dr. Rader's test are failing as requried

---
+++ src/clueGame/BadConfigFormatException.java
@@ -0,0 +1,11 @@
+package clueGame;
+
+public class BadConfigFormatException extends Exception {
+
+	public BadConfigFormatException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
