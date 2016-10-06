3b055f740b48dff1cbf0040c0850d14f3d41ccd4
basic <reecehughes@mymail.mines.edu>
2016-10-06 12:22:46 -0600

    Implementing Load Room Config.  Not Done, code not working.

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -2,84 +2,107 @@ package clueGame;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Board {
-	
+
 	private static Board boardInstance = new Board();
-	
+
 	private int numRows;
 	private int numColumns;
 	public static int MAX_BOARD_SIZE;
-	
+
 	private BoardCell[][] board;
-	
+
 	private Map<Character, String> rooms;
 	private Map<BoardCell, Set<BoardCell>> adjMatrix;
 	private Set<BoardCell> targets;
-	
+
 	private String boardConfigFile;
 	private String roomConfigFile;
-	
+
 	private Board() {
 		// Singleton
+		rooms = new HashMap<Character, String>();
+		adjMatrix = new HashMap<BoardCell, Set<BoardCell>>();
+		targets = new HashSet<BoardCell>();
+		
 	}
-	
-	public static Board getInstance(){
+
+	public static Board getInstance() {
 		return boardInstance;
 	}
-	
-	public void initialize(){
-		
+
+	public void initialize() {
+
 		loadRoomConfig();
 		loadBoardConfig();
 		calcAdjacencies();
-		
-		
+
 		return;
 	}
-	
-	public void loadRoomConfig(){
+
+	public void loadRoomConfig() {
+
+		String room = "";
+		char initial = ' ';
+		String[] fields = new String[3];
 		
-		String currentCell = "";
-		
+		FileReader reader = null;
+
 		try {
-			FileReader reader = new FileReader(roomConfigFile);
+			reader = new FileReader(roomConfigFile);
 		} catch (FileNotFoundException e) {
 			System.out.println("Room Configuration File Read Error");
 			e.printStackTrace();
+			return;
 		}
-		
+
 		Scanner in = new Scanner(reader);
-		
-		currentCell = in.next;
-		
-		
+
+		while (in.hasNext()) {
+			room = in.next();
+			
+			fields = room.split(",");
+			
+			// Get room initial
+			initial = fields[0].charAt(0);
+
+			// Get Room
+			room = fields[1];
+
+			// Add to rooms map
+			rooms.put(initial, room);
+			System.out.println("Added: " + room + ", " + initial + " to map.");
+		}
+
 		return;
 	}
-	
-	public void loadBoardConfig(){
-		
+
+	public void loadBoardConfig() {
+
 		return;
 	}
-	
-	public void calcAdjacencies(){
-		
+
+	public void calcAdjacencies() {
+
 		return;
 	}
-	
-	public void calcTargets(BoardCell cell, int pathLength){
-		
+
+	public void calcTargets(BoardCell cell, int pathLength) {
+
 		return;
 	}
 
 	public void setConfigFiles(String board, String rooms) {
-		
-		boardConfigFile = board;
-		roomConfigFile = rooms;
-		
+
+		boardConfigFile = "configFiles/" + board;
+		roomConfigFile = "configFiles/" + rooms;
+
 		return;
 	}
 
@@ -90,15 +113,13 @@ public class Board {
 	public int getNumRows() {
 		return numRows;
 	}
-	
+
 	public int getNumColumns() {
 		return numColumns;
 	}
-	
-	public BoardCell getCellAt(int r, int c){
+
+	public BoardCell getCellAt(int r, int c) {
 		return board[r][c];
 	}
-	
-	
 
 }
