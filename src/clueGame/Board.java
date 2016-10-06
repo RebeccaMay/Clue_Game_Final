9ee2296d93238ae33ac3a4615ea05fd5b8d1abcc
hujohnso <hujohnso@mymail.mines.edu>
2016-10-06 12:17:00 -0600

    Minor changes to LoarBoardConfig

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -7,79 +7,95 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Board {
-	
+
 	private static Board boardInstance = new Board();
-	
+
 	private int numRows;
 	private int numColumns;
 	public static int MAX_BOARD_SIZE;
-	
+
 	private BoardCell[][] board;
-	
+
 	private Map<Character, String> rooms;
 	private Map<BoardCell, Set<BoardCell>> adjMatrix;
 	private Set<BoardCell> targets;
-	
+
 	private String boardConfigFile;
 	private String roomConfigFile;
-	
+
 	private Board() {
 		// Singleton
 	}
-	
+
 	public static Board getInstance(){
 		return boardInstance;
 	}
-	
+
 	public void initialize(){
-		
+
 		loadRoomConfig();
 		loadBoardConfig();
 		calcAdjacencies();
-		
-		
+
+
 		return;
 	}
-	
+
 	public void loadRoomConfig(){
-		
+
 		String currentCell = "";
-		
+
 		try {
 			FileReader reader = new FileReader(roomConfigFile);
 		} catch (FileNotFoundException e) {
 			System.out.println("Room Configuration File Read Error");
 			e.printStackTrace();
 		}
-		
+
 		Scanner in = new Scanner(reader);
-		
+
 		currentCell = in.next;
-		
-		
+
+
 		return;
 	}
-	
+
 	public void loadBoardConfig(){
+		String currentCell = "";
+		FileReader reader = null;
+		try {
+			reader = new FileReader(boardConfigFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("board Configuration File Read Error");
+			e.printStackTrace();
+		}
+		Scanner in = new Scanner(reader);
+		while(in.hasNext()){
+			currentCell = currentCell + in.next();	
+		}
 		
+		
+		
+
+
 		return;
 	}
-	
+
 	public void calcAdjacencies(){
-		
+
 		return;
 	}
-	
+
 	public void calcTargets(BoardCell cell, int pathLength){
-		
+
 		return;
 	}
 
 	public void setConfigFiles(String board, String rooms) {
-		
+
 		boardConfigFile = board;
 		roomConfigFile = rooms;
-		
+
 		return;
 	}
 
@@ -90,15 +106,15 @@ public class Board {
 	public int getNumRows() {
 		return numRows;
 	}
-	
+
 	public int getNumColumns() {
 		return numColumns;
 	}
-	
+
 	public BoardCell getCellAt(int r, int c){
 		return board[r][c];
 	}
-	
-	
+
+
 
 }
