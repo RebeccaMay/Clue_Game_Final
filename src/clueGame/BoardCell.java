84d1389d41afde20153ba9fc67def7516e7b97c1
Andrew Petersen <andy.p@comcast.net>
2016-11-06 11:23:45 -0700

    added draw method to BoardCell, addevariables to BoardCell pertaining to the GUI

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,9 @@
 package clueGame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.lang.reflect.Field;
+
 public class BoardCell {
 	private int row;
 	private int column;
@@ -10,6 +14,11 @@ public class BoardCell {
 	private boolean room = false;
 	private boolean doorway = false;
 
+	//GUI related variables
+	Color c;
+	int sideLength;
+	String roomName = "";
+
 	public BoardCell(int row, int column, char initial,DoorDirection door) {
 		super();
 		this.row = row;
@@ -18,13 +27,16 @@ public class BoardCell {
 		this.door = door;
 		if(initial == 'W') {
 			walkway = true;
+			c = convertColor("yellow");
 		}
 		else if(door != DoorDirection.NONE){
 			doorway = true;
 			room = true;
+			c = convertColor("lightGray");
 		}
 		else{
 			room = true;
+			c = convertColor("lightGray");
 		}		
 	}
 
@@ -46,6 +58,28 @@ public class BoardCell {
 
 	public char getInitial() {
 		return initial;
+	}	
+
+	//setter for cell roomName string. Room name is a label drawn on the board for the given room
+	public void setRoomName(String s){
+		this.roomName = s;
 	}
 
+	// Private method for converting string representation of color to actual color object
+	private Color convertColor(String strColor) {
+		strColor = strColor.toLowerCase();
+		Color color; 
+		try {     
+			// We can use reflection to convert the string to a color
+			Field field = Class.forName("java.awt.Color").getField(strColor.trim());     
+			color = (Color)field.get(null); 
+		} catch (Exception e) {  
+			color = null; // Not defined  
+		}
+		return color;
+	}
+
+	public void draw(Graphics g){
+
+	}
 }
